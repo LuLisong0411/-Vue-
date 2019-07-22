f8f32623d6e1817f045910937b0ca1b25c485a77
陆力松 <lulisong0411@163.com>
2019-07-22 19:53:25 +0800

    注册页面的编写

---
+++ src/components/pages/Register.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+       <van-nav-bar
+        class="header"
+        title="用户注册"
+        left-text="返回"
+        left-arrow
+        @click-left="goBack()"
+        />
+        <div class="register-panel">
+            <van-field
+            v-model="userName"
+            type="userName"
+            label="用户名"
+            icon="clear"
+            placeholder="请输入用户名"
+            required
+            />
+
+            <van-field
+            v-model="password"
+            type="password"
+            label="密码"
+            placeholder="请输入密码"
+            required
+            />
+            <div class="register-button">
+                <van-button type="primary" size="large">马上注册</van-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                userName: '',
+                password: '',
+            }
+        },
+        methods: {
+            goBack() {
+                this.$router.go(-1)   
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .register-panel{
+        width:96%;
+        border-radius: 5px;
+        margin:20px auto;
+        padding-bottom:50px;
+    }
+    .register-button{
+        padding-top:10px;
+    }
+    /* .header{
+        background-color: #fa21f6;
+        color:floralwhite
+    } */
+</style>
--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ import router from './router'
 // Vue user(Vant)
 //按需引入  打包后体积较小，速度快
 
-import {Button,Row,Col,Swipe,SwipeItem,Lazyload,List} from 'vant'
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List)
+import {Button,Row,Col,Swipe,SwipeItem,Lazyload,List,Field,NavBar} from 'vant'
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar)
 
 Vue.config.productionTip = false
 
