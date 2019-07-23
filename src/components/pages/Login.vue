d88b7f51e1ece0b400057bdd75f4f75629d755ae
陆力松 <lulisong0411@163.com>
2019-07-23 17:40:13 +0800

    Vue的登录界面制作和路由配置

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,30 +1,29 @@
 <template>
     <div>
        <van-nav-bar
-        class="header"
-        title="用户注册"
+        title="用户登录"
         left-text="返回"
         left-arrow
         @click-left="goBack()"
         />
         <div class="register-panel">
             <van-field
-            v-model="username"
-            type="username"
-            label="用户名"
-            icon="clear"
-            placeholder="请输入用户名"
-            required
-            @click-icon="username=''":error-message="usernameErrorMsg"
+                v-model="username"
+                label="用户名"
+                icon="clear"
+                placeholder="请输入用户名"
+                required
+                @click-icon="username = ''"
+                :error-message="usernameErrorMsg"
             />
 
             <van-field
-            v-model="password"
-            type="password"
-            label="密码"
-            placeholder="请输入密码"
-            required
-            :error-message="passwordErrorMsg"
+                v-model="password"
+                type="password"
+                label="密码"
+                placeholder="请输入密码"
+                :error-message="passwordErrorMsg"
+                required
             />
             <div class="register-button">
                 <van-button type="primary" size="large" @click="registerAction" :loading="openLoading">马上注册</van-button>
@@ -57,6 +56,7 @@ import { Toast } from 'vant'
             //*********axios注册用户方法********
             axiosRegisterUser(){
                 this.openLoading = true;
+
                 axios({
                     url:url.registerUser,
                     method:'post',
@@ -65,23 +65,13 @@ import { Toast } from 'vant'
                         password: this.password,
                     }
                 }).then(response=>{
-                    console.log(response)
-                    //如果返回code为200，代表注册成功，我们给用户作Toast提示
-                    if(response.data.code == 200){
-                        Toast.success(response.data.message);
-                        this.$router.push('/')
-                    }else{
-                        console.log(response.data.message);
-                        Toast.fail('注册失败');
-                        this.openLoading = false
-                    }
+                    
+                    
                 }).catch((error)=>{
-                    console.log(error);
-                    Toast.fail('注册失败');
-                    this.openLoading = false
+                    
                 })
             },
-            // 编写验证方法checkFrom,用来专门验证表单信息
+            // **** 客户端验证:编写验证方法checkFrom,用来专门验证表单信息
             checkForm(){
                 let isOk = true;
                 if(this.username.length<5){
