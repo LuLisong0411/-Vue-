107ab4aa4a898b45a11b01d0e650cc8ec2587bf3
陆力松 <lulisong0411@163.com>
2019-07-25 19:32:06 +0800

    商品详情的页面模板编写完成

--- src/components/pages/Goods.vue
+++ src/components/pages/Goods.vue
@@ -1,5 +1,5 @@
 <template>
-   <div>
+<div>
     <div class="navbar-div">
         <van-nav-bar
             title="商品详情"
@@ -12,43 +12,60 @@
         <img :src="goodsInfo.IMAGE1" width="100%"/>
     </div>
     <div class="goods-name">{{goodsInfo.NAME}}</div>
-    <div class="goods-price">价格：{{goodsInfo.PRESENT_PRICE}}</div>
+    <div class="goods-price">价格 ：￥{{goodsInfo.PRESENT_PRICE | moneyFilter}}元</div>
     <div>
-        <van-tabs>
+        <van-tabs >
         <van-tab title="商品详情">
-            <div class="detail" v-html="goodsInfo.DETAIL">
+          <div class="detail" v-html="goodsInfo.DETAIL">
              
-            </div>
+          </div>
         </van-tab>
         <van-tab title="评价">
             正在制作中
         </van-tab>
         </van-tabs>
     </div>
+    <div class="goods-bottom">
+    
+    <div>
+        <van-button size="large" type="primary">加入购物车</van-button>
+    </div>
+    <div>
+        <van-button size="large" type="danger">直接购买</van-button>
+    </div>
+    
+</div>
 </div>
 </template>
 
 <script>
     import axios from 'axios'
-     import {Toast} from 'vant'
     import url from '@/serviceAPI.config.js'
+    import { Toast } from 'vant'
+    import {toMoney} from '@/filter/moneyFilter.js'
     export default {
         data() {
             return {
-                goodsId:'',
-                goodsInfo:{},  //商品详细信息 
+                goodsId: '',
+                goodsInfo:{},   //商品详细数据
             }
         },
+        filters:{
+            moneyFilter(money){
+                return toMoney(money)
+            }  
+        },
         created(){
-            
+            this.goodsId= this.$route.query.goodsId
+            console.log(this.goodsId)
             this.getInfo()
         },
         methods: {
             onClickLeft(){
                 this.$router.go(-1)
             },
+
             getInfo() {
-                
                 axios({
                     url:url.getDetailGoodsInfo,
                     method:'post',
@@ -57,13 +74,12 @@
                     }
                 })
                 .then(response=>{
-                    console.log(response)
-                    if(response.data.code== 200 && response.data.message){
-                        this.goodsInfo= response.data.message
+                    if(response.data.code == 200 && response.data.message ){
+                         this.goodsInfo = response.data.message 
                     }else{
-                        Toast('服务器错误，数据获取失败')
+                        Toast('服务器错误，数据取得失败')
                     }
-                     console.log(this.goodsInfo)
+                    console.log( this.goodsInfo)
                 })
                 .catch(error=>{
                     console.log(error)
@@ -74,5 +90,27 @@
 </script>
 
 <style scoped>
-
+    .goods-name{
+        background-color: #fff;
+    } 
+    .goods-price{
+        background-color: #fff;
+    }
+    .detail {
+        font-size:0px;
+    }
+    .goods-bottom{
+        position:fixed;
+        bottom:0px;
+        left:0px;
+        background-color: #FFF;
+        width:100%;
+        display: flex;
+        flex-direction: row;
+        flex-flow:nowrap;
+    }
+    .goods-bottom > div {
+        flex:1;
+        padding:5px;
+    }
 </style>
