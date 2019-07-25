34c8de7a99e33b03e65cc5307cf39377edeacee8
陆力松 <lulisong0411@163.com>
2019-07-25 18:56:46 +0800

    商品详情的页面模板编写

--- src/components/pages/Goods.vue
+++ src/components/pages/Goods.vue
@@ -1,16 +1,28 @@
 <template>
-    <div>
-        商品详情页面
+   <div>
+    <div class="navbar-div">
+        <van-nav-bar
+            title="商品详情"
+            left-text="返回"
+            left-arrow
+            @click-left="onClickLeft"
+        />
     </div>
+    <div class="topimage-div">
+        <img :src="goodsInfo.IMAGE1" width="100%"/>
+    </div>
+</div>
 </template>
 
 <script>
     import axios from 'axios'
+     import {Toast} from 'vant'
     import url from '@/serviceAPI.config.js'
     export default {
         data() {
             return {
-                goodsId: '775e575ce28a4f89b1dfe2c99eb08ae7'
+                goodsId:'',
+                goodsInfo:{},  //商品详细信息 
             }
         },
         created(){
@@ -18,7 +30,11 @@
             this.getInfo()
         },
         methods: {
+            onClickLeft(){
+                this.$router.go(-1)
+            },
             getInfo() {
+                
                 axios({
                     url:url.getDetailGoodsInfo,
                     method:'post',
@@ -28,6 +44,12 @@
                 })
                 .then(response=>{
                     console.log(response)
+                    if(response.data.code== 200 && response.data.message){
+                        this.goodsInfo= response.data.message
+                    }else{
+                        Toast('服务器错误，数据获取失败')
+                    }
+                     console.log(this.goodsInfo)
                 })
                 .catch(error=>{
                     console.log(error)
